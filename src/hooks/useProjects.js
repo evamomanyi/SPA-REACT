42e9d3e2cb0506fad1eaaa37283f283b324fa23c
evamomanyi <evanyaks13@gmail.com>
2026-06-01 16:25:16 +0300

    last edit

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -3,22 +3,22 @@ import { useState } from "react";
 function useProjects() {
   const [projects, setProjects] = useState([
     {
-      id: 1,
+      id: 101520240000,
       title: "Nem Mobile Banking Application",
       description: "A new mobile banking application that allows customers tobank from anywhere."
     },
     {
-      id: 2,
+      id: 121720251300,
       title: "PAPSS Enhancement",
       description: "A project to improve the current setup"
     },
     {
-      id: 3,
+      id: 31520261535,
       title: "Daily NSE report publishing project",
       description: "A portal for publishing daily NSE reports for customers to access"
     },
     {
-      id: 4,
+      id: 111620260400,
       title: "Sales Representatives Application Project",
       description: "A project to build an application that helps sales representatives to collect data from the field."
     }
